b2fbf59530a1123a4fafcbd767b07f4f1e368327
深圳鲲鹏 <apple@teacher.local>
2013-12-11 14:32:34 +0800

    first commit

---
+++ mymap/aaa.m
@@ -0,0 +1,31 @@
+//
+//  aaa.m
+//  mymap
+//
+//  Created by 深圳鲲鹏 on 13-12-11.
+//  Copyright (c) 2013年 tanghuacheng.cn. All rights reserved.
+//
+
+#import "aaa.h"
+
+@implementation aaa
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
